1dce6dae93351adea755a15f19f9fce4a263ce8d
Pham Van Lam <phamvanlam1991@gmail.com>
2019-09-05 16:43:00 +0700

    first commit

---
+++ src/main/java/com/springboot/unittest/SpringbootUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUnitTestApplication.class, args);
+	}
+
+}
